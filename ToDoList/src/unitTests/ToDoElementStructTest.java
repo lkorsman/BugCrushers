702ba706c329b33b8087019a5a3e3831ba9726a8
VeeVee <veeveew@gmail.com>
2019-04-21 14:16:32 -0700

    added test cases for ints

--- ToDoList/src/unitTests/ToDoElementStructTest.java
+++ ToDoList/src/unitTests/ToDoElementStructTest.java
@@ -88,23 +88,101 @@ class ToDoElementStructTest {
 		assertNotNull(td);	
 	}
 	
+	// Testing Constructor int parameter 
+	
 	/**
 	 * Test 5
 	 * 
-	 * Test String parameter in ctor 
-	 * Given: String with non-English characters
-	 * Expect: not null object
+	 * Test int parameter in ctor 
+	 * Given: negative int (-1)
+	 * Expect: Priority equals -1
 	 */
 	@Test
-	void ToDoElementStruct_ctorStringCompareNonEnglishTxt_notNull() {
+	void ToDoElementStruct_ctorIntNegative_EqualsNegOne() {
 		
-		String txt = "\u0400\u9986\u9994\u10084"; 		// Non-English characters in Unicode
+		String txt = "to do item"; 		
+		int prio = -1; 
+		LocalDate da = LocalDate.now(); 
+		boolean bool = false; 
+		
+		ToDoElementStruct td = new ToDoElementStruct(txt, prio, da, bool);
+		assertEquals(prio, td.getPriority());	
+	}
+	
+	/**
+	 * Test 6
+	 * 
+	 * Test int parameter in ctor 
+	 * Given: priority of zero
+	 * Expect: Priority equals 0
+	 */
+	@Test
+	void ToDoElementStruct_ctorIntZero_EqualsZero() {
+		
+		String txt = "to do item"; 		
 		int prio = 0; 
 		LocalDate da = LocalDate.now(); 
 		boolean bool = false; 
 		
 		ToDoElementStruct td = new ToDoElementStruct(txt, prio, da, bool);
-		assertEquals(txt, td.getToDoText());	
+		assertEquals(prio, td.getPriority());	
+	}
+	
+	/**
+	 * Test 7
+	 * 
+	 * Test int parameter in ctor 
+	 * Given: Priority equal to min value (1)
+	 * Expect: Priority equals 1
+	 */
+	@Test
+	void ToDoElementStruct_ctorIntLMinVal_Equals1() {
+		
+		String txt = "to do item"; 		
+		int prio = 1; 
+		LocalDate da = LocalDate.now(); 
+		boolean bool = false; 
+		
+		ToDoElementStruct td = new ToDoElementStruct(txt, prio, da, bool);
+		assertEquals(prio, td.getPriority());	
+	}
+	
+	/**
+	 * Test 8
+	 * 
+	 * Test int parameter in ctor 
+	 * Given: Priority equal to max value (5)
+	 * Expect: Priority equals 5
+	 */
+	@Test
+	void ToDoElementStruct_ctorIntLMaxVal_Equals5() {
+		
+		String txt = "to do item"; 		
+		int prio = 5; 
+		LocalDate da = LocalDate.now(); 
+		boolean bool = false; 
+		
+		ToDoElementStruct td = new ToDoElementStruct(txt, prio, da, bool);
+		assertEquals(prio, td.getPriority());	
+	}
+	
+	/**
+	 * Test 9
+	 * 
+	 * Test int parameter in ctor 
+	 * Given: Priority equal to max GUI value + 1 (6)
+	 * Expect: Priority equals 6
+	 */
+	@Test
+	void ToDoElementStruct_ctorIntLMaxGuiVal_Equals6() {
+		
+		String txt = "to do item"; 		
+		int prio = 6; 
+		LocalDate da = LocalDate.now(); 
+		boolean bool = false; 
+		
+		ToDoElementStruct td = new ToDoElementStruct(txt, prio, da, bool);
+		assertEquals(prio, td.getPriority());	
 	}
 	
 
